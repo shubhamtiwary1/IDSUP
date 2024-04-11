8c89ed2448e512a23d6f1728d499d150547537d3
shubhamtiwary1 <114869477+shubhamtiwary1@users.noreply.github.com>
2024-04-11 22:16:04 +0530

    Update project1.py

--- IDSUP/project1.py
+++ IDSUP/project1.py
@@ -4,6 +4,30 @@
 Created on Wed Feb 21 10:30:41 2024
 
 @author: student
+
+Data Science Project 1 ------------------------------------------->>>>>>>>
+
+users = [
+    { "id": 0, "name": "Hero" },
+    { "id": 1, "name": "Dunn" },
+    { "id": 2, "name": "Sue" },
+    { "id": 3, "name": "Chi" },
+    { "id": 4, "name": "Thor" },
+    { "id": 5, "name": "Clive" },
+    { "id": 6, "name": "Hicks" },
+    { "id": 7, "name": "Devin" },
+    { "id": 8, "name": "Kate" },
+    { "id": 9, "name": "Klein" },
+   
+]
+friendship_pairs = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4), (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+
+1.	Creation of DataSciencester Network
+2.	Find the total number of connections and average connections of the network
+3.	Sort from “most friends” to “least friends”
+4.	Find the friends of friends
+
+
 """
 from collections import OrderedDict
 users = [
